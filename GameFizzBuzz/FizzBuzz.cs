8e372e98825df958f4e6e538ca282484d26fa86f
KrzywdzinskiGrzegorz <krzywdzinskigrzegorz@gmail.com>
2022-11-02 23:33:25 +0100

    Add code to Main method and create GetResult method in FizzBuzz class.

--- GameFizzBuzz/FizzBuzz.cs
+++ GameFizzBuzz/FizzBuzz.cs
@@ -8,5 +8,18 @@ namespace GameFizzBuzz
 {
     internal class FizzBuzz
     {
+        public string GetResult(int number)
+        {
+            if (number % 3 == 0 && number % 5 == 0)
+                return "FizzBuzz";
+
+            if (number % 3 == 0)
+                return "Fizz";
+
+            if (number % 5 == 0)
+                return "Buzz";
+
+            return number.ToString();
+        }
     }
 }
--- GameFizzBuzz/Program.cs
+++ GameFizzBuzz/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,54 @@ namespace GameFizzBuzz
     {
         static void Main(string[] args)
         {
+			try
+			{
+				FizzBuzz fizzBuzz = new FizzBuzz();
 
+				Console.WriteLine("Gra FizzBuzz\nAby zakończyć grę wpisz liczbę 112.");
+
+				while (true)
+				{
+					Console.WriteLine("Podaj liczbę:");
+
+                    var number = GetNumber();
+
+					//zakończenie gry, przerwane pętli gdy użytkownik wprowadzi wartość 112
+					if (number == 112)
+						break;
+
+                    Console.WriteLine(fizzBuzz.GetResult(number));
+                }
+				
+
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
         }
+
+		static int GetNumber()
+		{
+			try
+			{
+				while (true)
+				{
+					if (!int.TryParse(Console.ReadLine(), out var number))
+					{
+						Console.WriteLine("Wprowadziłeś błedne dane !!!\nPodaj liczbę:");
+						//jeżeli uzytkownik wprowadzi błędne dane, to instrukcja continuee spowoduje 
+						//że dalsza część kodu nie bedzie wykonywana, wrócimy do początku pętli 
+						continue;
+					}
+					return number;
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+				return 0;
+			}
+		}
     }
 }
